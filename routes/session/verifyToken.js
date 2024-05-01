7b6e5e212d201158489227236ce5a144399748bb
DaBr16 <daniel.brezer@gmail.com>
2024-05-01 16:02:50 +0200

    First commit

---
+++ routes/session/verifyToken.js
@@ -0,0 +1,24 @@
+const dotenv = require('dotenv');
+const jwt = require('jsonwebtoken');
+
+dotenv.config();
+const verifyToken = async (req, res, next) => {
+  console.log("Test:", req.query)
+  const token = req.cookies.token || '';
+  try {
+    if (!token) {
+      return res.status(401).json('you need to login')
+    }
+    const decrypt = await jwt.verify(token, process.env.TOKEN_SECRET);
+    req.user = {
+      email: decrypt.email,
+      id:decrypt.id
+    };
+    console.log("Token verified")
+    next();
+  } catch (err) {
+    return res.status(500).json(err.toString());
+  }
+};
+
+module.exports = verifyToken;
